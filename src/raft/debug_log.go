3becb9f52bedf0cfa26acbc1abacb19b3198247f
Andrew-wong-ty <isandrew.t.y.wong@gmail.com>
2024-04-26 15:13:09 -0400

    Update verbose level of Raft

--- src/raft/debug_log.go
+++ src/raft/debug_log.go
@@ -55,7 +55,7 @@ func init() {
 }
 
 func DebugLog(topic logTopic, rf *Raft, format string, a ...interface{}) {
-	if debugVerbosity >= 1 {
+	if debugVerbosity >= 2 {
 		currTime := time.Since(debugStart).Microseconds()
 		currTime /= 100
 		prefix := fmt.Sprintf("%06d %v ", currTime, string(topic))
--- src/raft/run-with-debug-logs.sh
+++ src/raft/run-with-debug-logs.sh
@@ -1,3 +1,3 @@
 # reference: https://blog.josejg.com/debugging-pretty/
-VERBOSE=1  go test -run TestInitialElection2A  > output.log
+VERBOSE=2  go test -run TestInitialElection2A  > output.log
 /usr/bin/python3 dslogs.py -c 3  output.log > out.log
